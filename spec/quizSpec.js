39a9c2cf20f2b06b572f5ce4157f0b84434fc4da
mvanger <mvanger@nyu.edu>
2013-08-22 09:58:22 -0400

    Done

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -23,5 +23,62 @@ describe('question1', function() {
 
 });
 
+describe('question2', function() {
+
+  it('returns true if number is 93', function() {
+    expect(quiz.nearHundred(93)).toBe(true);
+  });
+
+  it('returns false if number is 52', function() {
+    expect(quiz.nearHundred(52)).toBe(false);
+  });
+
+  it('throws an error for non-numbers', function() {
+    expect(quiz.nearHundred('two')).toBe("Error: Please enter a number!");
+  });
+
+});
+
+describe('question3', function(){
+
+  it('returns the split string', function(){
+    expect(quiz.missingChar("kittie", 1)).toBe("kttie");
+  });
+
+  it('throws an error without a string', function(){
+    expect(quiz.missingChar(347, 1)).toBe("Error: Please enter a string!");
+  });
+
+});
+
+describe('question4', function(){
+
+  it('deletes del from a string', function(){
+    expect(quiz.delDel("abdelcd")).toBe("abcd");
+  });
+
+  it('does not change anything without del', function(){
+    expect(quiz.delDel("xyz")).toBe("xyz");
+  });
+
+});
+
+describe('question5', function(){
+
+  it('moves the last character to the front', function(){
+    expect("cat".backAround()).toBe("tca");
+  });
+
+  it('moves the last character to the front', function(){
+    expect("hello".backAround()).toBe("ohell");
+  });
+
+});
 // Write the next tests yourself!
 // See quiz.js for more details
+
+
+
+
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -12,15 +12,34 @@ var quiz = {};
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
 quiz.sleepIn = function(options) {
-}
+  date = Date();
+  date = date.substring(0, 3);
+  console.log(options);
+  if (date === "Sat" || date === "Sun") {
+    return true;
+  } else if (options && options.vacation === true){
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Question 2: function called nearHundred
 //////////////
 // If the number is between 90 and 99, the result is true;
 // If it is 89 or below, it is false.
 // quiz.nearHundred(52) => false
-// quiz.nearHundred(93) => false
+// quiz.nearHundred(93) => false TRUE??
 // quiz.nearHundred('two') => Error: Please enter a number!
+quiz.nearHundred = function(number) {
+  if (89 < number && number < 100) {
+    return true;
+  } else if (typeof(number) !== "number") {
+    return "Error: Please enter a number!";
+  } else {
+    return false;
+  }
+};
 
 // Question 3: a function called missingChar
 //////////////
@@ -28,15 +47,34 @@ quiz.sleepIn = function(options) {
 // If you don't enter a string
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
+quiz.missingChar = function(string, index) {
+  if (typeof(string) !== "string") {
+    return "Error: Please enter a string!";
+  } else {
+  return string.slice(0, index) + string.slice(index+1);
+  }
+};
 
 // Question 4: a function called delDel
 //////////////
 // Remove "del" from a string.
 // quiz.delDel("abdelcd") => "abcd"
 // quiz.delDel("xyz") => "xyz"
+quiz.delDel = function(string){
+  return string.replace("del", "");
+};
 
 // Question 5: a method called backAround
 //////////////
 // Given a string, move the last character to the beginning.
 // "cat".backAround() => "tca"
 // "hello".backAround() => "ohell"
+String.prototype.backAround = function(string){
+  return this.slice(-1) + this.substr(0, this.length -1);
+};
+
+
+
+
+
+
